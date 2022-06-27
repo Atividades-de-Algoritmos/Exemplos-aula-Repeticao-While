5db7cc8e33b49981615c3454c8d1efcef5b1114a
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-27 07:06:04 -0300

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -15,28 +15,28 @@
 ################   1   #########################
 numero = int(input('Digite um número inteiro positivo: '))
 
-fat = 1
-while numero > 0:
-  fat = fat * numero
-  numero = numero - 1
-print('O fatorial desse número é ', fat)
+fat = 1 # inicializar o fatorial com 1
+while numero > 0: # criar um laço para calcular o fatorial do número informado pelo usuário
+  fat = fat * numero # multiplicar o fatorial pelo número informado pelo usuário
+  numero = numero - 1 # decrementar o número informado pelo usuário
+print('O fatorial desse número é ', fat) # imprimir o fatorial do número informado pelo usuário
 
 ################   2   ###########################
 # se a entrada for ZERO
 #entrada
-num1 = int(input("informe um número :"))
-temp= num1
-fat = 1
-#processamento
-if num1 == 0 :
-  pass
-else:
-  while (num1 > 0):
-    fat = fat * num1 # 5*4*3*2*1
-    num1 = num1 - 1
+num1 = int(input("informe um número :")) # ler o valor e armazenar em uma variável valor1
+temp = num1 # armazenar o valor informado pelo usuário em uma variável temporária
+fat = 1 # inicializar o fatorial com 1 para o caso de ser ZERO
+#processamento de dados
+if num1 == 0 : # se o número informado pelo usuário for ZERO
+  pass # não fazer nada
+else: # se o número informado pelo usuário não for ZERO
+  while (num1 > 0): # criar um laço para calcular o fatorial do número informado pelo usuário
+    fat = fat * num1 # multiplicar o fatorial pelo número informado pelo usuário
+    num1 = num1 - 1 # decrementar o número informado pelo usuário
 
 #saida
-print(f"o fatorial de {temp} é {fat}")
+print(f"o fatorial de {temp} é {fat}") # imprimir o fatorial do número informado pelo usuário e armazenado na variável fat
 
 
 
