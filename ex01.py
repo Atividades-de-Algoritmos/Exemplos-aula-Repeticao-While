53c999a083a46b1810102a0050f2f0bb5839a309
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-13 21:39:13 -0300

    Números pares entre 100 e 200 📡

--- ex01.py
+++ ex01.py
@@ -1,42 +1,61 @@
 #
-#
-# autores:
+# Autores:
 # Michel Silva
+# Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 13/07/2022
 #
 # 1 - Programa que imprime a quantidade de números
 # pares de 100 até 200, incluindo-os.
-#
-# usando o laço While
-# entrada de dados
-num = 100 # criar uma variável para armazenar o número inicial
-contadorPares = 0 # criar uma variável para armazenar a quantidade de números pares entre 100 e 200
-while num <= 200: # criar um laço para calcular a quantidade de números pares entre 100 e 200 incluindo-os
-  if num % 2 == 0: # se o resto da divisão do número informado pelo usuário por 2 for igual a 0 (número par)
-    contadorPares = contadorPares + 1 # incrementar a quantidade de números pares informados pelo usuário + 1
-  num = num + 1 # ficar atento a identação do bloco para evitar um loop infinito
-print(contadorPares) # imprimir a quantidade de números pares entre 100 e 200 incluindo-os
-print("fim do programa")
+
+# [Usando o laço While]
+
+# Entrada de dados
+
+num = 100 # Criando uma variável para armazenar o número inicial.
+contadorPares = 0 # Criando uma variável para armazenar a quantidade de números pares entre 100 e 200.
+
+# Processamento de dados
+
+while num <= 200: # Criando um laço while, enquanto variavel 'num' for menor igual que 200 o código irá executar
+  
+  if num % 2 == 0: # Se o número é múltiplo de 2 temos que o número é par
+    contadorPares = contadorPares + 1 # Incrementar a quantidade de números pares informados pelo usuário + 1
+  
+  else: # Caso contrário irá executar o comando pass que somente passa um ciclo do nosso while
+    pass
+
+  num = num + 1 # Ficar atento a identação do bloco para evitar um loop infinito
+
+# Saída de dados
+
+print(f'Quantidade de números pares entre 100 e 200: {contadorPares}') # Imprimindo a quantidade de números pares entre 100 e 200 incluindo-os
+print("\nfim do programa")
 
 
+# Versão 2.0 do código
 
+# -------------------------------------------------- #
 
-###############################################################################
-print("###############################################################################")
+# [Usando o laço FOR]
 
-# usando o laço FOR:
-# entrada de dados
-quantidade = 0 # criar uma variável para armazenar a quantidade de números pares
+"""# Entrada de dados
 
-# processamento de dados
-for i in range(100, 201): # criar um laço para calcular a quantidade de números pares de 100 até 200
-    if i % 2 == 0: # se o resto da divisão do número informado pelo usuário por 2 for igual a 0
-        print(i) # imprimir o número informado pelo usuário
-        quantidade += 1 # incrementar a quantidade de números pares informados pelo usuário
-    else: # se o resto da divisão do número informado pelo usuário por 2 não for igual a 0 (número impar)
-        continue # continuar o laço de repetição
+quantidade = 0 # Criando uma variável para armazenar a quantidade de números pares
 
-# saida de dados
-print(f"A quantidade de números pares de 100 até 200 é {quantidade}")
-print("fim do programa")
+# Processamento de dados
+
+for i in range(100, 201): # Criando um laço para calcular a quantidade de números pares de 100 até 200 passando-os como parâmetro pra função range()
+    
+    if i % 2 == 0: # Se o resto da divisão do número informado pelo usuário por 2 for igual a 0
+        print(f'{i}', end = ' ') # Imprimindo o número informado pelo usuário
+        quantidade += 1 # Incrementar a quantidade de números pares informados pelo usuário
+    
+    else: # Caso contrário sabemos que nosso número é impar, então executa a indentação
+        continue # Volta pro início do for
+
+# Saída de dados
+
+print(f"\n\nA quantidade de números pares de 100 até 200: {quantidade}")
+print("\nfim do programa")
+"""
