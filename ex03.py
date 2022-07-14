0ed88549080533d55f7bd2c8544078f36cb3edd8
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-13 21:40:13 -0300

    Somando Pares ➗2️⃣

--- ex03.py
+++ ex03.py
@@ -1,50 +1,75 @@
 #
-#
-# autores:
+# Autores:
 # Michel Silva
+# Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 13/07/2022
 #
 # 3 - Programa que imprime a soma de todos os
 # números pares entre dois números quaisquer,
 # incluindo-os.
-#
-# usando o laço While:
-# entrada de dados
-num1 = int(input('Entre com o valor inicial: ')) # ler o valor e armazenar em uma variável valor1
-num2 = int(input('Entre com o valor final: '))  # ler o valor e armazenar em uma variável valor2
 
-soma = 0 # criar uma variável para armazenar a soma dos números pares entre os valores informados pelo usuário
-while num1 <= num2: # criar um laço para somar os números pares entre os valores informados pelo usuário
-  if num1 % 2 == 0: # se o resto da divisão do número informado pelo usuário por 2 for igual a 0
-    soma = soma + num1 # somar o número informado pelo usuário a variável soma e armazenar na variável soma
-  num1 = num1 + 1  # ficar atento a identação do bloco para evitar um loop infinito (não precisa ficar no final do laço)
-print(f"A soma é {soma}") # imprimir a soma dos números pares entre os valores informados pelo usuário e armazenada na variável soma
-print('fim do programa')
+# [ Usando o laço While ]
+
+# -- Entrada de dados --
+
+num1 = int(input('Entre com o valor inicial: ')) # lendo o valor e armazenando em uma variável num1
+num2 = int(input('Entre com o valor final: '))  # lendo o valor e armazenando em uma variável num2
+soma = 0 # Criando uma variável para armazenar a soma dos números pares entre os valores informados pelo usuário
+valor_inicial = num1 # Criando um cópia de num1 para usar no print, já que irá ser alterado no decorrer do código
+
+# -- Processamento de dados --
+
+print() # Deixando um espaço no terminal, como um enter do teclado
+
+while num1 <= num2: # Criando um laço para somar os números pares entre os valores informados pelo usuário
+  
+  if num1 % 2 == 0: # Se o resto da divisão do número informado pelo usuário por 2 for igual a 0
+    print(f'{num1}', end = ' ')
+    soma = soma + num1 # Somando o número informado pelo usuário a variável soma
+  
+  else: # Caso contrário sabemos que o numéro é ímpar
+    pass # Nome 'pass' é bem intuítivo, ele apenas passa um ciclo do código while, é um comando omisso
+
+  num1 = num1 + 1  # Ficar atento a identação do bloco para evitar um loop infinito (não precisa ficar no final do laço)
+
+# -- Saída de dados --
+
+print(f"\n\nA soma dos números pares entre {valor_inicial} e {num2}: {soma}") # Imprimindo a soma dos números pares entre os valores informados pelo usuário
+print('\nfim do programa') # Informando ao usuário que o programa terminou
 
 
+# Versão 2.0 do código
 
+# -------------------------------------------------------- #
 
+"""# By Carlos Eduardo
 
-#############################################################################
-print("#############################################################################")
+# [ Usando o laço FOR ]
 
+# -- Entrada de dados --
 
-#usando o laço FOR:
-# entrada de dados
-valor1 = int(input("informe um valor: ")) # ler o valor e armazenar em uma variável valor1
-valor2 = int(input("informe um valor: ")) # ler o valor e armazenar em uma variável valor2
-soma = 0 # criar uma variável para armazenar a soma dos números pares
+valor1 = int(input("informe um valor: ")) # lendo o valor e armazenando em uma variável valor1
+valor2 = int(input("informe um valor: ")) # lendo o valor e armazenando em uma variável valor2
+soma = 0 # Criando uma variável para armazenar a soma dos números pares
 
-# processamento de dados
-for i in range(valor1, valor2 + 1): # criar um laço para calcular a soma dos números pares entre os valores informados pelo usuário
-    if i % 2 == 0: # se o resto da divisão do número informado pelo usuário por 2 for igual a 0
-        print(i) # imprimir o número informado pelo usuário
-        soma += i # incrementar a soma dos números pares informados pelo usuário
-    else: # se o resto da divisão do número informado pelo usuário por 2 não for igual a 0 (número impar)
-        continue # continuar o laço de repetição
+# -- Processamento de dados --
 
-# saida de dados
-print(f"A soma dos números pares entre {valor1} e {valor2} é {soma}") # imprimir a soma dos números pares entre os valores informados pelo usuário
-print("fim do programa")
+print() # Print vazio deixa um espaço em branco no terminal, como um enter do teclado
 
+for i in range(valor1, valor2 + 1): # Criando um laço para calcular a soma dos números pares entre os valores informados pelo usuário
+    
+    if i % 2 == 0: # Se o resto da divisão do número informado pelo usuário por 2 for igual a 0 sabemos que é par
+        
+        print(f'{i}', end = ' + ') if i != (valor2) else print(i, end = '')  # Imprimindo o número informado pelo usuário
+        soma += i # Incrementando a soma dos números pares informados pelo usuário
+    
+    else: # Se o resto da divisão do número informado pelo usuário por 2 não for igual a 0 sabemos que é número impar
+        continue # Volta pro início do for só que agora em outro ciclo
+
+# -- Saída de dados --
+
+print(f"\n\nA soma dos números pares entre {valor1} e {valor2}: {soma}") # imprimir a soma dos números pares entre os valores informados pelo usuário
+print("\nfim do programa")"""
+
+# -------------------------------------------------------- #
